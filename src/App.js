fc3cca29de4769b9c01f92d3435546c4fcac27f9
Nathan Gearing <natgearing@gmail.com>
2023-10-14 21:59:52 +1100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TickTack from "./pages/1-ticktacktoe";
+
+function App() {
+	return (
+		<>
+			<h1>React Projects</h1>
+			<TickTack />
+		</>
+	);
+}
+
+export default App;
---
+++ src/pages/1-ticktacktoe.js
@@ -0,0 +1,5 @@
+export default function TickTack() {
+	return (
+		<div className="flex flex-col items-center justify-between">X Col</div>
+	);
+}
