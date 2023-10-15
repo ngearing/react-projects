6f9c89cf0080bae9f18ef17b9413ebf8064c5566
Nathan Gearing <natgearing@gmail.com>
2023-10-15 21:58:43 +1100

    add - x&o turns

--- src/pages/1-ticktacktoe.jsx
+++ src/pages/1-ticktacktoe.jsx
@@ -1,33 +1,77 @@
+import { useState } from "react";
 import "./1-style.css";
 
+// https://react.dev/learn/tutorial-tic-tac-toe
+
 export default function TickTack() {
+	// setup state for each square
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	// set players turn
+	const [xIsNext, setXIsNext] = useState(true);
+
+	function handleClick(i) {
+		// check square for value
+		if (squares[i]) {
+			return;
+		}
+
+		const nextSquares = squares.slice();
+		nextSquares[i] = xIsNext ? "X" : "O";
+		setSquares(nextSquares);
+		setXIsNext(!xIsNext); // switch player
+	}
+
 	return (
 		<>
 			<div className="board-row">
-				<Square value="1" />
-				<Square value="2" />
-				<Square value="3" />
+				<Square
+					value={squares[0]}
+					onSquareClick={() => handleClick(0)}
+				/>
+				<Square
+					value={squares[1]}
+					onSquareClick={() => handleClick(1)}
+				/>
+				<Square
+					value={squares[2]}
+					onSquareClick={() => handleClick(2)}
+				/>
 			</div>
 			<div className="board-row">
-				<Square value="4" />
-				<Square value="5" />
-				<Square value="6" />
+				<Square
+					value={squares[3]}
+					onSquareClick={() => handleClick(3)}
+				/>
+				<Square
+					value={squares[4]}
+					onSquareClick={() => handleClick(4)}
+				/>
+				<Square
+					value={squares[5]}
+					onSquareClick={() => handleClick(5)}
+				/>
 			</div>
 			<div className="board-row">
-				<Square value="7" />
-				<Square value="8" />
-				<Square value="9" />
+				<Square
+					value={squares[6]}
+					onSquareClick={() => handleClick(6)}
+				/>
+				<Square
+					value={squares[7]}
+					onSquareClick={() => handleClick(7)}
+				/>
+				<Square
+					value={squares[8]}
+					onSquareClick={() => handleClick(8)}
+				/>
 			</div>
 		</>
 	);
 }
 
-function Square({ value }) {
-	function handleClick() {
-		console.log("You clicked on id", value);
-	}
+function Square({ value, onSquareClick }) {
 	return (
-		<button className="square" onClick={handleClick}>
+		<button className="square" onClick={onSquareClick}>
 			{value}
 		</button>
 	);
