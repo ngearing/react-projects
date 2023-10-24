bafa6902309a6db6343649726ff18c7c2c085ef8
Nathan Gearing <natgearing@gmail.com>
2023-10-24 21:50:40 +1100

    Add - ticktack history

--- src/pages/1-ticktacktoe.jsx
+++ src/pages/1-ticktacktoe.jsx
@@ -4,13 +4,19 @@ import "./1-style.css";
 // https://react.dev/learn/tutorial-tic-tac-toe
 
 export default function TickTack() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const currentSquares = history[history.length - 1];
+	const [currentMove, setCurrentMove] = useState(0);
+	const currentSquares = history[currentMove];
+	const xIsNext = currentMove % 2 === 0;
 
 	function handlePlay(squares) {
-		setXIsNext(!xIsNext);
-		setHistory([...history, squares]);
+		const nextHistory = [...history.slice(0, currentMove + 1), squares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
+	}
+
+	function jumpTo(move) {
+		setCurrentMove(move);
 	}
 
 	return (
@@ -21,7 +27,7 @@ export default function TickTack() {
 				squares={currentSquares}
 				handlePlay={handlePlay}
 			/>
-			<BoardInfo history={history} handlePlay={handlePlay} />
+			<BoardInfo history={history} jumpTo={jumpTo} />
 		</div>
 	);
 }
@@ -56,37 +62,69 @@ function Board({ xIsNext, squares, handlePlay }) {
 		<div className="board">
 			<div className="status">
 				{status}
-				{(winner || noTurns) && <button onClick={resetBoard}>Reset</button>}
+				{(winner || noTurns) && (
+					<button onClick={resetBoard}>Reset</button>
+				)}
 			</div>
 			<div className="board-row">
-				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+				<Square
+					value={squares[0]}
+					onSquareClick={() => handleClick(0)}
+				/>
+				<Square
+					value={squares[1]}
+					onSquareClick={() => handleClick(1)}
+				/>
+				<Square
+					value={squares[2]}
+					onSquareClick={() => handleClick(2)}
+				/>
 			</div>
 			<div className="board-row">
-				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+				<Square
+					value={squares[3]}
+					onSquareClick={() => handleClick(3)}
+				/>
+				<Square
+					value={squares[4]}
+					onSquareClick={() => handleClick(4)}
+				/>
+				<Square
+					value={squares[5]}
+					onSquareClick={() => handleClick(5)}
+				/>
 			</div>
 			<div className="board-row">
-				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+				<Square
+					value={squares[6]}
+					onSquareClick={() => handleClick(6)}
+				/>
+				<Square
+					value={squares[7]}
+					onSquareClick={() => handleClick(7)}
+				/>
+				<Square
+					value={squares[8]}
+					onSquareClick={() => handleClick(8)}
+				/>
 			</div>
 		</div>
 	);
 }
 
-function BoardInfo({ history, handlePlay }) {
+function BoardInfo({ history, jumpTo }) {
 	return (
 		<div className="info">
 			<ol>
-				{history.map((step, move) => {
-					const desc = step ? `Go to move #${move + 1}` : "Go to game start";
-					<li>
-						<button>{desc}</button>
-					</li>;
-				})}
+				{history.map((step, move) => (
+					<li key={move}>
+						<button onClick={() => jumpTo(move)}>
+							{move !== 0
+								? `Go to move #${move}`
+								: "Go to game start"}
+						</button>
+					</li>
+				))}
 			</ol>
 		</div>
 	);
@@ -113,7 +151,11 @@ function calculateWinner(squares) {
 	];
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
 			return squares[a];
 		}
 	}
